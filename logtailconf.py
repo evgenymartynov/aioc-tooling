d048e414dab5692f942ef55f15011e7581193ebc
Ubuntu <ubuntu@ip-10-240-11-212.ap-southeast-2.compute.internal>
2014-07-02 09:48:38 +0000

    Initial commit

---
+++ logtailconf.py
@@ -0,0 +1,48 @@
+COLOURS = [
+  "#000000",
+  "#000080",
+  "#008000",
+  "#FF0000",
+  "#804040",
+  "#8000FF",
+  "#808000",
+  "#008080",
+  "#00FFFF",
+  "#0000FF",
+  "#FF00FF",
+  "#808080",
+  "#C0C0C0",
+]
+
+PROLOGUE = '''\
+<html>
+<head>
+  <style>
+    body {
+        font-family: monospace, Monospace, 'Courier New';
+        font-size: smaller;
+    }
+
+    .timestamp {
+        color: #bbb;
+    }
+
+    .server {
+        color: #a22;
+    }
+
+    .nick {
+        color: #555;
+    }
+
+    .links-top * {
+      color: #00c;
+    }
+  </style>
+</head>
+<body>
+<p class="links-top">
+  [ <a href="aioc">small</a> | <a href="aioc-large">large</a> ]
+</p>'''
+
+EPILOGUE = '</body></html>'
